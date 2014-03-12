21bc02e66bb25c551b878d77b3ff4f28cf881e5a
Alejandro Pineda <alejandro.deathscythe@gmail.com>
2014-03-12 11:27:33 -0600

    Test

--- Twitter/Ejemplo1.1.py
+++ Twitter/Ejemplo1.1.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Mining the social web 2.0
 
@@ -48,9 +49,11 @@ lo tiene yahoo, un api, registrarse
 
 _mundo_woe_id=1
 _usa_woe_id=23424977
+_mexico_woe_id=23424900
 
 _mundo_tt=twitter_api.trends.place(_id=_mundo_woe_id)
 _usa_tt=twitter_api.trends.place(_id=_usa_woe_id)
+_mexico_tt=twitter_api.trends.place(_id=_mexico_woe_id)
 
 #Se regresan diccionarios
 
@@ -75,6 +78,8 @@ print _mundo_tt
 print
 print _usa_tt
 print
+print _mexico_tt
+print
 
 """
 Ejemplo 1.3 Mostrar las respuestas de la API como JSON con formato
@@ -86,6 +91,8 @@ print json.dumps(_mundo_tt, indent=1)
 print
 print json.dumps(_usa_tt, indent=1)
 print
+print json.dumps(_mexico_tt, indent=1)
+print
 
 """
 Ejemplo 1.4 Calculando la interseccion de dos sets de TT
@@ -94,8 +101,156 @@ Ejemplo 1.4 Calculando la interseccion de dos sets de TT
 
 _set_mundo=set([trend['name'] for trend in _mundo_tt[0]['trends']])
 _set_usa=set([trend['name'] for trend in _usa_tt[0]['trends']])
+_set_mexico=set([trend['name'] for trend in _mexico_tt[0]['trends']])
 
-_tt_comun=_set_mundo.intersection(_set_usa)
-
-print _tt_comun
+print "Trending Topics"
 print
+print 'Mexico',_set_mexico
+print
+print 'US',_set_usa
+print
+print 'Mundo',_set_mundo
+print
+
+_tt_comun_mundo_usa=_set_mundo.intersection(_set_usa)
+_tt_comun_mundo_mexico=_set_mundo.intersection(_set_mexico)
+_tt_comun_mexico_usa=_set_mexico.intersection(_set_usa)
+_tt_comun_mundo_usa_mexico=_set_mundo.intersection(_set_usa,_set_mexico)
+
+print "Intersecciones"
+print
+print "Mundo y US",_tt_comun_mundo_usa
+print
+print "Mundo y Mexico",_tt_comun_mundo_mexico
+print
+print "Mundo, Mexico y Us",_tt_comun_mundo_usa_mexico
+print
+print "Mexico y US",_tt_comun_mexico_usa
+print
+
+"""
+Ejemplo 1.5 Guardando resultados de busquedas
+
+La variable q pueder utilizada para buscar no solo hashtags sino cualquier
+paralabra o lo que quieras buscar.
+"""
+
+#Hastag que voy a buscar.
+
+q="mujerlunabella"
+
+count = 100
+
+search_results=twitter_api.search.tweets(q=q, count = count)
+
+statuses= search_results['statuses']
+
+#Iteramos por otros 5 grupos de resultados siguiendo el cursor.
+for _ in range(5):
+    print "Tamaño de los status", len(statuses)
+    try:
+        next_results=search_results['search_metadata']['next_results']
+        #El campo search_metada tambien contiene  un valor refresh_url que puede
+        #utilizarse para mantener y actualizar los resultados con info nueva
+    except KeyError, e:
+        break
+        
+    #Se crea un diccionario de los siguientes resultados
+    
+    kwargs=dict([kv.split('=') for kv in next_results[1:].split('&')])
+    
+    #Se utiliza *args y **kwargs como parametros en una funcion, estos son idiomas
+    #de python que sirven para expresar argumentos arbitrarios y argumentos de 
+    #palabras claves arbitrarios, respectivamente
+    
+    search_results=twitter_api.search.tweets(**kwargs)
+    statuses+=search_results['statuses']
+    
+print json.dumps(statuses[0], indent=1)
+
+"""
+Ejemplo 1.6 Extrallendo texto, username y #
+
+List comprehensions se utiliza mucho
+"""
+
+
+status_texts=[status['text'] for status in statuses]
+
+screen_names=[user_mention['screen_name'] for status in statuses for user_mention in status['entities']['user_mentions']]
+
+hashtags=[hashtag['text'] for status in statuses for hashtag in status['entities']['hashtags']]
+
+#Calcula una coleccion de todas las palabras de todos los tweets
+words = [w for t in status_texts for w in t.split()]
+
+#Explorar los primeros 5 items
+print json.dumps(status_texts[0:5],indent=1)
+print json.dumps(screen_names[0:5],indent=1)
+print json.dumps(hashtags[0:5], indent=1)
+print json.dumps(words[0:5],indent=1)
+
+"""
+Ejemplo 1-7 Creando una distribucion de frecuencia basica en base a las 
+palabras en los tweets
+"""
+
+from collections import Counter
+
+for item in [words, screen_names, hashtags]:
+    c = Counter(item)
+    print c.most_common()[:10] #Top 10
+    print
+    
+"""
+Ejemplo 1-8 Utilizando prettytable para mostrar las tuplas en una forma bonita
+
+"""
+
+from prettytable import PrettyTable
+
+for label, data in (('Word', words),('Screen Name', screen_names),('Hashtag', hashtags)):
+    pt=PrettyTable(field_names=[label, 'Count'])
+    c=Counter(data)
+    [pt.add_row(kv) for kv in c.most_common()[:10]] #Top 10
+    pt.align[label], pt.align['Count']='l','r' #Alineacion de las columnas
+    print pt
+
+"""
+Ejemplo 1-9 Calculando la diversidad lexica para los tweets
+
+"""
+
+#Una funcion para calcular la diversidad lexica
+
+def lexical_diversity(tokens):
+    return 1.0*len(set(tokens))/len(tokens)
+
+def average_words(statuses):
+    total_words=sum([len(s.split()) for s in statuses])
+    return 1.0*total_words/len(statuses)
+
+print
+print "lexical diversity words", lexical_diversity(words)
+print "lexical diversity screen names", lexical_diversity(screen_names)
+print "lexical diversity hash", lexical_diversity(hashtags)
+print "average words status texts", average_words(status_texts)
+print
+
+"""
+Ejemplo 1-10 Encontrar los retweets mas populares
+"""
+
+#Guarda en una tupla estos tres valores por cada status mientras cumpla la 
+#condicion
+retweets=[(status['retweet_count'], status['retweeted_status']['user']['screen_name'],status['text']) for status in statuses if status.has_key('retweeted_status')]
+
+#Mostramos solo los primeros 5 resultados y mostramos los primeros items en 
+#cada tupla
+
+pt= PrettyTable(field_names=['Count', 'Screen Name', 'Text'])
+
+[pt.add_row(row) for row in sorted(retweets, reverse=True)[:5]]
+pt.max_width['Text']=50
+pt.align='l'
+print pt
--- Twitter/TT_laguna.py
+++ Twitter/TT_laguna.py
@@ -19,17 +19,61 @@ auth=twitter.oauth.OAuth(_oauth_token,_oauth_token_secret,_consumer_key,_consume
 
 twitter_api=twitter.Twitter(auth=auth)
 
+
 #2. Obtener TT de la laguna y Mexico (Pais)
+try:
+    """
+    Twitter aun no soporta localizacion de TT via woeid para algunos paises. 
+    Encontrar otra solucion
+    """
+    _mexico_woe_id=23424900
+    _gomez_woe_id=12599957
+    _torreon_woe_id=12599920
+    _lerdo_woe_id=12599962
+    _lalaguna_woe_id=90317919
+    _durango_woe_id=2346273
+    _coahuila_woe_id=2346270
 
-_mexico_woe_id=23424900
-_torreon_woe_id=149893
-_gomez_woe_id=124050
-_lerdo_woe_id=116557
-_lalaguna_woe_id=90317919
+    _mexico_tt=twitter_api.trends.place(_id=_mexico_woe_id)
+    #_coauila_tt=twitter_api.trends.place(_id=_coahuila_woe_id)
+    #_durango_tt=twitter_api.trends.place(_id=_durango_woe_id)
+    #_torreon_tt=twitter_api.trends.place(_id=_torreon_woe_id)
+    #_gomez_tt=twitter_api.trends.place(_id=_gomez_woe_id)
+    #_lerdo_tt=twitter_api.trends.place(_id=_lerdo_woe_id)
+    #_lalaguna_tt=twitter_api.trends.place(_id=_lalaguna_woe_id)
+except twitter.TwitterHTTPError, e:
+    print "Error 404, no se encontro la locacion"
+    print
+    print e
+      
 
-_mexico_tt=twitter_api.trends.place(_id=_mexico_woe_id)
-_torreon_tt=twitter_api.trends.place(_id=_torreon_woe_id)
-_gomez_tt=twitter_api.trends.place(_id=_gomez_woe_id)
-_lerdo_tt=twitter_api.trends.place(_id=_lerdo_woe_id)
-_lalaguna_tt=twitter_api.trends.place(_id=_lalaguna_woe_id)
+#3. Mostrar TT en JSON
 
+import json
+print json.dumps(_mexico_tt, indent=1)
+print
+"""
+print json.dumps(_torreon_tt, indent=1)
+print
+print json.dumps(_gomez_tt, indent=1)
+print
+print json.dumps(_lerdo_tt, indent=1)
+print
+print json.dumps(_lalaguna_tt, indent=1)
+print
+
+#4.Crear los set y encontrar similitudes
+
+_set_mexico=set([trend['name'] for trend in _mexico_tt[0]['trends']])
+_set_torreon=set([trend['name'] for trend in _torreon_tt[0]['trends']])
+_set_gomez=set([trend['name'] for trend in _gomez_tt[0]['trends']])
+_set_lerdo=set([trend['name'] for trend in _lerdo_tt[0]['trends']])
+_set_lalaguna=set([trend['name'] for trend in _lalaguna_tt[0]['trends']])
+
+_tt_comun_mexico_laguna=_set_mexico.intersection(_set_lalaguna)
+_tt_comun_laguna=_set_lalaguna.intersection(_set_lerdo,_set_torreon,_set_gomez)
+
+print _tt_comun_mexico_laguna
+print
+print _tt_comun_laguna
+"""
